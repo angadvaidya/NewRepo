ee01a5eb38b64fdd50f646e916d35e3a1dbb2aac
unknown <angadvaiday@gmail.com>
2023-07-05 14:53:32 +0530

    file added

---
+++ SSC.java
@@ -0,0 +1,18 @@
+package collection;
+
+/*
+public class SSC {
+    private int sr_no;
+    private String F_name;
+    private String L_name;
+    private String address;
+    private int phone;
+
+}
+class Student{
+    public static void main(String[] args) {
+        List<student> sd=new ArrayList<>();
+    }
+
+}
+*/
